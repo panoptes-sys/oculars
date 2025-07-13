db3a6817cd7b6dfed8b55383e62632d56f1f1e52
stqcky <xxillik@gmail.com>
2025-07-13 15:46:44 +0300

    feat(needle): create a simple asm cli wrapper

---
+++ bin/needle/src/main.rs
@@ -0,0 +1,50 @@
+//! CLI utility for getting information about EVM instructions.
+
+/// CLI arguments.
+mod args;
+/// Mnemonic utilities.
+mod mnemonics;
+
+use anyhow::Context;
+use clap::Parser as _;
+use eva_asm::opcode::Mnemonic;
+use minus::Pager;
+use std::fmt::Write;
+
+use crate::{
+    args::{Cli, Command},
+    mnemonics::{get_mnemonic_by_name, MnemonicDescription},
+};
+
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Mnemonics => list_mnemonics()?,
+        Command::Mnemonic { name } => display_mnemonic_info_by_name(name)?,
+    }
+
+    Ok(())
+}
+
+/// List a list of all mnemonics.
+fn list_mnemonics() -> anyhow::Result<()> {
+    let mut pager = Pager::new();
+
+    for mnemonic in Mnemonic::iter() {
+        writeln!(pager, "{}\n", mnemonic.format_human())
+            .context("failed to write mnemonic info")?;
+    }
+
+    minus::page_all(pager).context("failed to display mnemonic info")?;
+
+    Ok(())
+}
+
+/// Display information about a mnemonic by its name.
+fn display_mnemonic_info_by_name(name: String) -> anyhow::Result<()> {
+    let mnemonic = get_mnemonic_by_name(&name)?;
+    println!("{}", mnemonic.format_human());
+
+    Ok(())
+}
