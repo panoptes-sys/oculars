9159d2af9fa647ada3e61602db94dc1a592cd1cf
stqcky <xxillik@gmail.com>
2025-08-10 13:49:06 +0300

    refactor(dasm): remove nightly features

--- crates/dasm/src/lib.rs
+++ crates/dasm/src/lib.rs
@@ -1,10 +1,6 @@
-#![allow(incomplete_features, reason = "it is what it is")]
-#![feature(generic_const_exprs)]
-#![feature(specialization)]
-
 //! EVM disassembler.
-// pub mod assembly;
-// pub mod bytecode;
-// pub mod disassembler;
-// // pub mod instruction;
-// pub mod source;
+
+pub mod assembly;
+pub mod bytecode;
+pub mod disassembler;
+pub mod source;
